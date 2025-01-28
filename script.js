446ddb82ad67769c604c0fe796dd8118bbd7e155
Riya Rastogi <riya02rastogi@gmail.com>
2025-01-28 19:51:23 +0530

    base

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
-git init
+function doubleTrouble(arr) {
+    if (arr.length === 0) {
+      return arr; 
+    }
+}
