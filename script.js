3113009152a86193aec37a947eb930a6df180132
Riya Rastogi <riya02rastogi@gmail.com>
2025-01-28 19:39:01 +0530

    ques

---
+++ script.js
@@ -0,0 +1,1 @@
+// The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
