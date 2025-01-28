a19317a554809dc36c543a0afe05a3253293f153
Riya Rastogi <riya02rastogi@gmail.com>
2025-01-28 19:59:27 +0530

    ans1

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
 function doubleTrouble(arr) {
     if (arr.length === 0) {
-      return arr; 
+        return arr;
     }
+    const result = [arr[0]];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === arr[i - 1]) {
+            result.push(arr[i]); 
+        } else {
+            result.push(arr[i] * 2); 
+        }
+    }
+    return result;
 }
+const arr = [1, 2, 2, 3, 4, 4, 5];
+console.log(doubleTrouble(arr));
